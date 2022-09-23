cb85e3c8605fcdd49d784d10a3ffd38df86b416f
Khamid-v2022 <khamid.webdev@gmail.com>
2022-09-23 15:23:14 +0800

    Michael Archangel page added

---
+++ application/views/michael_prayer.php
@@ -0,0 +1,43 @@
+
+<body>
+    <section class="main michael-prayer">
+        <div class="container">  
+            <div class="d-flex justify-content-center">          
+                <img width="180" height="81" src="<?=base_url()?>assets/img/agb-logo-skyblue.png" alt="">
+            </div>
+        
+            <div class="heading text-center">
+                <h1>The Most Powerful Prayer to Summon Archangel Haniel</h1>
+            </div>
+
+            <div class="form_wrapper bg-white shadow rounded mb-5">
+            	<div class="sub-headline">
+	            	<p>I discovered this prayer not long ago and I really liked how it sounded to summon the angel of joy. </p>
+	            	<p>	When I started saying it, a very strange series of events happened.</p>
+	            	<p>	Get this amazing prayer <strong>so you can listen to this over and over again.</strong></p>
+	            </div>
+	            <div class="topic-img-wrapper mt-4 mb-5">
+	            	<img src="<?=base_url()?>assets/img/bundle-small-min.png" loading="lazy" sizes="(max-width: 479px) 88vw, 300px" alt="" class="image-4">
+	            </div>
+	            <div class="submit-form-wrapper">
+	            	<form method="post" id="michael_pray_form">
+                        <input type="hidden" id="ip_address" value="">
+                        <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter your Name" autocomplete="off" required>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Enter your E-mail Address" autocomplete="off" required>
+
+                        <div class="text-center my-4">
+                            <button type="submit" class="btn btn-post-msg btn-primary" name="post_my_msg">Send me this Powerful Archangel Michael</button>
+                        </div>
+                        
+                    </form>
+                    <div class="loader d-none"></div>
+                    <div class="dpm-response d-none">
+                        <p class="alert alert-danger d-none">Email not valid - Please try with another email address!</p>
+                        <p class="alert alert-info d-none">Validating your contact details...</p>
+                        <p class="alert alert-success d-none">Valid Email address!</p>
+                    </div>
+	            </div>
+            </div>
+        </div>
+    </section>
+</body>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,27 @@
 $(function() { 
 
-    $('#datatable').DataTable({
-      order: [[0, 'desc']],
-      columnDefs: [{
-          "targets": [4, 5],
-          "orderable": false
-      }]
+    var key = "7967cf9c7b724f988614a8e4dad3ba1b";
+    var url = "https://ipgeolocation.abstractapi.com/v1/?api_key=" + key;
+    
+    $.getJSON(url, function(data) {
+        $("#ip_address").val(data.ip_address);
     });
     
-    $('#datatable').on('click', ".edit-btn", function(){
-        $("#m_note_id").val($(this).parents('tr').attr('note_id'));
-        $("#m_note").val($(this).parents('tr').attr('note'));
 
-        $("#requestModal").modal('show');
+    $("#pray_form").submit(function(e){
+        e.preventDefault();
+        $(".dpm-response").addClass('d-none');
+        $(".dpm-response .alert").addClass('d-none');
+        if (!event.target.checkValidity()) {
+            return false;
+        }
+        submit();
     })
 
-
-    $("#datatable").on('click', ".delete-btn", function(){
-        swal({
-          title: "Are you sure?",
-          text: "Once deleted, you will not be able to recover this imaginary file!",
-          icon: "warning",
-          buttons: true,
-          dangerMode: true,
-        })
-        .then((willDelete) => {
-          if (willDelete) {
-            var id = $(this).parents('tr').attr('note_id');
-            var url = site_url + 'manage/delete_note';
-            $.post(url, 
-                {
-                    id: id
-                }, function(resp){
-                    location.reload();
-            })
-          } 
-        });
-        
-    });
-
-
-
-    $("#m_form").submit(function(e){
+    $("#m_pray_form").submit(function(e){
         e.preventDefault();
-       
+        $(".dpm-response").addClass('d-none');
+        $(".dpm-response .alert").addClass('d-none');
         if (!event.target.checkValidity()) {
             return false;
         }
@@ -51,17 +29,172 @@ $(function() {
         submit_modal_form();
     })
 
+    $("#michael_pray_form").submit(function(e){
+        e.preventDefault();
+        $(".dpm-response").addClass('d-none');
+        $(".dpm-response .alert").addClass('d-none');
+        if (!event.target.checkValidity()) {
+            return false;
+        }
+        submit_michael_form();
+    })
+    
+    $(".card_main").on('click', function(){
+        var _url = site_url + "oraclecard/random_article";
+        location.href = _url;
+    })
+
 });
 
-function submit_modal_form(){
-    var url = site_url + 'manage/update_note';
+function numberWithCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0]=parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
 
-    $.post(url, 
-        {
-            id: $("#m_note_id").val(),
-            note: $("#m_note").val()
-        }, function(resp){
-            location.reload();
+function submit(){
+    $(".request-form").addClass('d-none');
+    $(".loader").removeClass('d-none');
+
+    $(".dpm-response").removeClass('d-none');
+
+    // var email_verify_url = "https://disposable.debounce.io/?email=" + $("#email").val();
+    var email_verify_url = site_url + "welcome/verify_email";
+    
+    $(".alert-info").removeClass('d-none');
+    $.post(email_verify_url, {
+        email: $("#email").val()
+    }, function(resp){ 
+        console.log(resp);
+        if(resp == "Valid"){ 
+            $(".dpm-response .alert").addClass('d-none');
+            $(".dpm-response .alert-success").removeClass('d-none');
+
+            let current_url = window.location;
+            let params = (new URL(current_url)).searchParams;
+            let tid = params.get('tid');
+            
+            var url = site_url + 'welcome/submit_pray';
+            
+            $.post(url, {
+                    ip_address: $("#ip_address").val(),
+                    email: $("#email").val(),
+                    first_name: $("#first_name").val(),
+                    note: $("#note").val(),
+                    is_publish: $('#display').prop('checked')?"yes":"no",
+                    tag: tid
+                }, 
+                function(resp){
+                    // $(".loader").addClass('d-none');
+                    // $(".request-form").removeClass('d-none');
+                    // if(resp == 'ok'){
+                        // window.location.href = "https://angelgraceblessing.com/prayer-thank-you/";
+                    // }
+                    // else{
+                    //     $(".request-form").removeClass('d-none');
+                    //     // $('.alert-danger').html("");
+                    //     // $(".dpm-response").removeClass('d-none');
+                    // }
+            })
+            window.location.href = "https://angelgraceblessing.com/prayer-thank-you/";
+        } else {
+            $(".loader").addClass('d-none');
+            $(".request-form").removeClass('d-none');
+            
+            $(".dpm-response .alert").addClass('d-none');
+            $(".dpm-response .alert-danger").removeClass('d-none');
+        }
+    })   
+}
+
+
+function submit_modal_form(){
+    $(".request-form").addClass('d-none');
+    $(".loader").removeClass('d-none');
+    $(".dpm-response").removeClass('d-none');
+ 
+    // var email_verify_url = "https://disposable.debounce.io/?email=" + $("#m_email").val();
+    var email_verify_url = site_url + "welcome/verify_email";
+
+    $.post(email_verify_url, {
+        email: $("#m_email").val()
+    }, function(resp){ 
+        if(resp== "Valid"){ 
+            $(".dpm-response .alert").addClass('d-none');
+            $(".dpm-response .alert-success").removeClass('d-none');
+
+            let current_url = window.location;
+            let params = (new URL(current_url)).searchParams;
+            let tid = params.get('tid');
+
+            var url = site_url + 'welcome/submit_pray';
+            
+            $.post(url, {
+                    ip_address: $("#ip_address").val(),
+                    email: $("#m_email").val(),
+                    first_name: $("#m_first_name").val(),
+                    note: $("#m_note").val(),
+                    is_publish: $('#m_display').prop('checked')?"yes":"no",
+                    tag: tid
+                }, 
+                function(resp){
+                    // $(".loader").addClass('d-none');
+                    // $(".request-form").removeClass('d-none');
+                    // if(resp == 'ok'){
+                    //     window.location.href = "https://angelgraceblessing.com/prayer-thank-you/";
+                    // }
+                    // else{
+                    //     // $('.alert-danger').html("");
+                    //     // $(".dpm-response").removeClass('d-none');
+                    // }
+            })
+            window.location.href = "https://angelgraceblessing.com/prayer-thank-you/";
+        } else {
+            $(".loader").addClass('d-none');
+            $(".request-form").removeClass('d-none');
+            
+            $(".dpm-response .alert").addClass('d-none');
+            $(".dpm-response .alert-danger").removeClass('d-none');
+        }
     })
 }
 
+function submit_michael_form(){
+    $("#michael_pray_form").addClass('d-none');
+    $(".loader").removeClass('d-none');
+
+    $(".dpm-response").removeClass('d-none');
+
+    var email_verify_url = site_url + "welcome/verify_email";
+    
+    $(".alert-info").removeClass('d-none');
+    $.post(email_verify_url, {
+        email: $("#email").val()
+    }, function(resp){ 
+        if(resp == "Valid"){ 
+            $(".dpm-response .alert").addClass('d-none');
+            $(".dpm-response .alert-success").removeClass('d-none');
+
+            let current_url = window.location;
+            let params = (new URL(current_url)).searchParams;
+            let tid = params.get('tid');
+            
+            var url = site_url + 'michaelPrayer/submit_pray';
+            
+            $.post(url, {
+                    ip_address: $("#ip_address").val(),
+                    email: $("#email").val(),
+                    first_name: $("#first_name").val(),
+                    tag: tid
+                }, 
+                function(resp){
+            })
+            window.location.href = "https://angelgraceblessing.com/prayer-thank-you/";
+        } else {
+            $(".loader").addClass('d-none');
+            $(".request-form").removeClass('d-none');
+            $(".dpm-response .alert").addClass('d-none');
+            $(".dpm-response .alert-danger").removeClass('d-none');
+        }
+    })   
+}
