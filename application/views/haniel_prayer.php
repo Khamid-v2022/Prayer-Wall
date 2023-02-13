a0d0a2b1250ccd52664a4f84aa01afc406f1dbd3
Khamid-v2022 <khamid.webdev@gmail.com>
2023-02-13 16:18:44 +0800

    change the layout for Haniel Prayer page

--- application/views/haniel_prayer.php
+++ application/views/haniel_prayer.php
@@ -2,40 +2,39 @@
 <body class="michael-prayer">
     <section class="main">
         <div class="container">  
-            <div class="heading text-center mt-5">
-                <h1>The Most Powerful Prayer to Summon Archangel Haniel</h1>
+            <div class="text-center mt-5">
+                <h1>Start Receiving <span class="text-danger">Unexpected Wealth & Miracles</span> Using This Powerful Prayer To Summon Archangel Haniel</h1>
             </div>
-
-            <div class="form_wrapper bg-white shadow rounded mt-5 mb-5">
-                <div class="haniel-description my-5 mx-3">
-                	<div class="sub-headline px-5">
-    	            	<p>I discovered this prayer not long ago and I really liked how it sounded to summon the angel of joy. <br>
-    	            	When I started saying it, a very strange series of events happened.
-    	            	Get this amazing prayer <strong>so you can listen to this over and over again.</strong></p>
-    	            </div>
-                    <div class="topic-img-wrapper ">
-                        <img src="<?=base_url()?>assets/img/bundle-small-min.png" loading="lazy" sizes="(max-width: 479px) 88vw, 300px" alt="" class="image-4">
-                    </div>
-    	            
+            <div class="haniel-description my-5">
+                <div class="topic-img-wrapper mt-5 mb-5">
+                    <img src="<?=base_url()?>assets/img/bundle-small-min.png" loading="lazy" sizes="(max-width: 479px) 88vw, 300px" alt="" class="image-4">
                 </div>
-	            <div class="submit-form-wrapper">
-	            	<form method="post" id="michael_pray_form">
-                        <input type="hidden" id="ip_address" value="">
-                        <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter your Name" autocomplete="off" required>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Enter your E-mail Address" autocomplete="off" required>
+                <div class="form_wrapper bg-white shadow rounded ms-4 p-4">
+                	<div class="sub-headline">
+                        <h4>Recite This Simple Prayer to Receive Immediate Help</h4><br>
+    	            	<p>I discovered this prayer not long ago and I really liked how it sounded to summon the angel of joy.<br><br>
+                        When I started recite it, a very strange series of events happened.<br><br>
+                        Get this amazing prayer so you can listen to this over and over again.</p>
+    	            </div>
+                    <div class="mt-5" style="max-width: 400px; margin: 0 auto;">
+    	            	<form method="post" id="michael_pray_form">
+                            <input type="hidden" id="ip_address" value="">
+                            <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter your Name" autocomplete="off" required>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter your E-mail Address" autocomplete="off" required>
 
-                        <div class="text-center my-4">
-                            <button type="submit" class="btn btn-post-msg btn-primary" name="post_my_msg">Send me this Powerful Archangel Haniel</button>
+                            <div class="text-center mt-4">
+                                <button type="submit" class="btn btn-post-msg btn-primary" name="post_my_msg">👉Download Now</button>
+                            </div>
+                            
+                        </form>
+                        <div class="loader d-none"></div>
+                        <div class="dpm-response d-none">
+                            <p class="alert alert-danger d-none">Email not valid - Please try with another email address!</p>
+                            <p class="alert alert-info d-none">Validating your contact details...</p>
+                            <p class="alert alert-success d-none">Valid Email address!</p>
                         </div>
-                        
-                    </form>
-                    <div class="loader d-none"></div>
-                    <div class="dpm-response d-none">
-                        <p class="alert alert-danger d-none">Email not valid - Please try with another email address!</p>
-                        <p class="alert alert-info d-none">Validating your contact details...</p>
-                        <p class="alert alert-success d-none">Valid Email address!</p>
                     </div>
-	            </div>
+                </div>
             </div>
         </div>
     </section>
