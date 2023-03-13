77cbc3e9ec84593342e47dc1402d789adc0ab248
Khamid-v2022 <khamid.webdev@gmail.com>
2023-03-13 16:47:19 +0800

    Added ten-commandments page

---
+++ application/views/ten_commandments.php
@@ -0,0 +1,51 @@
+    <section class="main ten-commandment">
+        <div class="google-ads-place">
+            <!-- angelgrace 10 commandments top -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-9926134965337187"
+                 data-ad-slot="2658001045"
+                 data-ad-format="auto"
+                 data-full-width-responsive="true"></ins>
+            <script>
+                 (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+
+
+        <div class="text-center py-5">
+            <h1 class="font-46" style="color: rgba(00, 204, 255)"><?=$title?></h2>
+        </div>
+        
+        <div class="container">  
+            <div class="main-content">
+                <div class="text-msg-wrapper">
+                    <ul class="text-msgs">
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-covet-anything-that-belongs-to-your-neighbor">You Shall Not Covet Anything That Belongs To Your Neighbor</a></li>
+                         <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-remember-the-sabbath-day-to-keep-it-holy">Remember the Sabbath day, To Keep It Holy</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-steal">You Shall Not Steal</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-commit-adultery">You Shall Not Commit Adultery</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-murder">You Shall Not Murder</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-honor-your-father-and-your-mother">Honor Your Father And Your Mother</a></li>
+                       
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-bear-false-witness-against-your-neighbor">You Shall Not Bear False Witness Against Your Neighbor</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-take-the-name-of-the-lord-your-god-in-vain">You Shall Not Take The Name Of The Lord Your God In Vain</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-make-idols">You Shall Not Make Idols</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-have-no-other-gods-before-me">You shall have no other gods before Me</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="google-ads-place">
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-format="autorelaxed"
+                 data-ad-client="ca-pub-9926134965337187"
+                 data-ad-slot="6920394275"></ins>
+            <script>
+                 (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+    </section>
+</body>
