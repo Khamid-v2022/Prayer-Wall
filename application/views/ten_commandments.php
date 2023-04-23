cebf8c45ed92346470b7e0d30fd7ee517a2eac51
Khamid-v2022 <khamid.webdev@gmail.com>
2023-04-24 07:13:26 +0800

    change the order of ten-commendents

--- application/views/ten_commandments.php
+++ application/views/ten_commandments.php
@@ -21,17 +21,16 @@
             <div class="main-content">
                 <div class="text-msg-wrapper">
                     <ul class="text-msgs">
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-covet-anything-that-belongs-to-your-neighbor">You Shall Not Covet Anything That Belongs To Your Neighbor</a></li>
-                         <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-remember-the-sabbath-day-to-keep-it-holy">Remember the Sabbath day, To Keep It Holy</a></li>
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-steal">You Shall Not Steal</a></li>
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-commit-adultery">You Shall Not Commit Adultery</a></li>
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-murder">You Shall Not Murder</a></li>
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-honor-your-father-and-your-mother">Honor Your Father And Your Mother</a></li>
-                       
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-bear-false-witness-against-your-neighbor">You Shall Not Bear False Witness Against Your Neighbor</a></li>
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-take-the-name-of-the-lord-your-god-in-vain">You Shall Not Take The Name Of The Lord Your God In Vain</a></li>
-                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-make-idols">You Shall Not Make Idols</a></li>
                         <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-have-no-other-gods-before-me">You shall have no other gods before Me</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-make-idols">You Shall Not Make Idols</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-take-the-name-of-the-lord-your-god-in-vain">You Shall Not Take The Name Of The Lord Your God In Vain</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-remember-the-sabbath-day-to-keep-it-holy">Remember the Sabbath day, To Keep It Holy</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-honor-your-father-and-your-mother">Honor Your Father And Your Mother</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-murder">You Shall Not Murder</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-commit-adultery">You Shall Not Commit Adultery</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-steal">You Shall Not Steal</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-bear-false-witness-against-your-neighbor">You Shall Not Bear False Witness Against Your Neighbor</a></li>
+                        <li><a class="post-link" href="https://angelgraceblessing.com/the-10-commandments-you-shall-not-covet-anything-that-belongs-to-your-neighbor">You Shall Not Covet Anything That Belongs To Your Neighbor</a></li>
                     </ul>
                 </div>
             </div>
